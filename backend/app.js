aafcd19b9cdf9555262bd32bb2a0ac8a487b4521
abiral-adhikari <109904208+abiral-adhikari@users.noreply.github.com>
2023-05-06 23:59:17 +0545

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -35,12 +35,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// Catch-all route that redirects all requests to the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
+// // Catch-all route that redirects all requests to the React app
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+// });
 
 //port listening to info
 const listen=() => {

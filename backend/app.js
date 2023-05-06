dffd420c397f00db1b2cc9cf07080cb444e1a211
abiral-adhikari <109904208+abiral-adhikari@users.noreply.github.com>
2023-05-06 22:00:36 +0545

    without port

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 // Allow requests from specific origin
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://projecthub-78g5.onrender.com/',
   optionsSuccessStatus: 200
 }));
 

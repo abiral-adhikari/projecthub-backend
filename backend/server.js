3b8f6299c41a7cf01b2779473ce18065a339bbd7
abiral-adhikari <109904208+abiral-adhikari@users.noreply.github.com>
2023-05-07 10:51:37 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -121,5 +121,5 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // Catch-all route to serve the React app's index.html file
 app.get('*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.status(404).sendFile(path.join(__dirname, '../frontend/build/index.html'));
   });

a83995f51b4621e05a272bdee62e2052bce26362
abiral-adhikari <109904208+abiral-adhikari@users.noreply.github.com>
2023-05-07 14:31:26 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,36 +3,16 @@ attach environment variables to process.env from .env file
 */
 require('dotenv').config();
 const http = require('http');//import http module
-// const app=require('./app');
-const path = require('path');
-const express = require('express');//import express module 
-const dbConnect=require('./database/database.js');
-const app =express();//creating express app
-const bodyparser=require('body-parser');
-const cors=require('cors');
+const app=require('./app');
 
-//for every page there is different 
 const userroute=require('./routes/user.js')
 const projectroute=require('./routes/project.js')
 const discussionroute=require('./routes/discussion.js')
 const assignmentroute=require('./routes/todo.js')
 const resourceroute=require('./routes/resource.js')
-
-// Set up your API routes here
-//route handling for 
+//route handling for js
 app.use('/api/user',userroute)
 app.use('/api/project',projectroute)
 app.use('/api/chat',discussionroute)
 app.use('/api/todo',assignmentroute)
-app.use('/api/resource',resourceroute)
-
-// // Serve static files from the React app's build directory
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// // Catch-all route to serve the React app's index.html file
-
-// app.get('/*', function (req, res) {
-//   console.log("Index.html")
-//   res.sendFile(path.join(__dirname, 'build/index.html'));
-// });
-
+app.use("/api/resource",resourceroute)
